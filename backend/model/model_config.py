bb5bc9ffc5ced4a04a9d3f305d79fdb23db98a78
Debora Taira Novaes <92901837+deborataira@users.noreply.github.com>
2024-11-20 20:33:47 -0300

    Revert "<Sprint III> <Feature>:<Histórico com o Login>"

--- backend/model/model_config.py
+++
@@ -1,8 +0,0 @@
-# model_config.py
-import google.generativeai as genai
-from config.config import API_KEY
-
-def configure_api():
-    genai.configure(api_key=API_KEY)
-
-
--- backend/services/sentiment_analysis.py
+++
@@ -1,9 +0,0 @@
-from textblob import TextBlob
-
-def get_sentiment(text):
-    analysis = TextBlob(text)
-    if analysis.sentiment.polarity > 0.1:
-        return 'positivo'
-    elif analysis.sentiment.polarity < -0.1:
-        return 'negativo'
-    return 'neutro'
